e72c0c6b8a3247827b1ef9270f4c58fcf63dd9ed
Andrew Li <andrewli@u.northwestern.edu>
2025-08-26 21:24:44 -0500

    add slackbot

--- bot-notifier/notifier/consumers/__init__.py
+++ bot-notifier/notifier/consumers/__init__.py
@@ -4,5 +4,6 @@ A Consumer registry that avoids long imports in main.py
 
 from .consumer import Consumer as Consumer
 from .discord_consumer import DiscordConsumer as DiscordConsumer
+from .slack_consumer import SlackConsumer as SlackConsumer
 
-consumers = [DiscordConsumer()]
+consumers = [DiscordConsumer(), SlackConsumer()]
